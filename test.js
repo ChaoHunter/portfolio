f4d2cb3e6d343bd10728801d2c2db3becc928f05
ChaoHunter <65525638+ChaoHunter@users.noreply.github.com>
2021-12-14 13:12:36 -0500

    Delete test.js
    made duplicate

--- test.js
+++
@@ -1,7 +0,0 @@
-$(window).scroll(function () {
-    if ($(window).scrollTop() >= 50) {
-    $('.navbar').css('background','hsla(229, 34%, 25%, 0.7)');
-    } else {
-    $('.navbar').css('background','transparent');
-    }
-    });
